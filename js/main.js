eeb91701932976ce484b6c345f44f1598e92f4e6
Rusline <salikov.rusline@gmail.com>
2013-09-06 13:24:14 +0600

    original

---
+++ js/main.js
@@ -0,0 +1,71 @@
+$(function() {
+	$('.marquee .item1').liMarquee({
+		direction: 'left',
+		loop:-1,			
+		scrolldelay: 0,	
+		scrollamount:50,	
+		circular: true,	
+		drag: false,
+		runshort:true
+	});
+	$('.marquee .item2').liMarquee({
+		direction: 'left',
+		loop:-1,			
+		scrolldelay: 0,	
+		scrollamount:80,	
+		circular: true,	
+		drag: false,
+		runshort:true
+	});
+	
+	//placeholder
+	if (!Modernizr.input.placeholder) {
+		$('input[placeholder], textarea[placeholder]').placeholder();
+	}
+	
+	$("input.phonenumber").mask("+9 (999) 999-99-99");
+	
+	// Clone
+	$('.add1').click(function() {
+		$(".clone1 .line").clone().appendTo(".clone-add1");
+		return false;
+	});
+	$('.add2').click(function() {
+		$(".clone2 .line").clone().appendTo(".clone-add2");
+		return false;
+	});
+	
+	
+	//to top
+	$('#toTop').click(function() {
+		$('html,body').animate({scrollTop:0},800);
+		return false;
+	});
+	
+	// Fancy
+	$('.fancybox').fancybox({
+		padding: 0,
+		openEffect  : 'none',
+		closeEffect : 'none',
+		closeBtn: false,
+		helpers : {
+			 title : {
+            type : 'inside'
+        }
+		}
+	});
+	
+	$('.fancybox-media').fancybox({
+		padding: 0,
+		openEffect  : 'none',
+		closeEffect : 'none',
+		closeBtn: false,
+		helpers : {
+			media : {},	
+			title : {
+        type : 'inside'
+      }
+		}
+	});
+	
+});
